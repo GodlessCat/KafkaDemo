bf89348052db3d64deb61e2e99f04f5bde1d22cd
Ilya Tsvyk <Godless.catt@gmail.com>
2021-07-13 13:29:22 +0300

    fix port issue

--- src/main/java/com/tsvyk/kafkademo/services/ConsumerService.java
+++ src/main/java/com/tsvyk/kafkademo/services/ConsumerService.java
@@ -1,13 +1,20 @@
 package com.tsvyk.kafkademo.services;
 
+import org.apache.kafka.clients.producer.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConsumerService {
 
+    private final Logger logger = LoggerFactory.getLogger(Producer.class);
+
     @KafkaListener(topics="my_topic", groupId="my_group_id")
     public void consume(String message){
-        System.out.println("Consuming the message: " + message);
+
+
+        logger.info(String.format("#### -> Consumed message -> %s", message));
     }
 }
