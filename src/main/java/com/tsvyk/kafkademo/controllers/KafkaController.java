30c34a55afdb12b423c13ef7475c04428648883f
Ilya Tsvyk <Godless.catt@gmail.com>
2021-07-13 12:06:54 +0300

    add app to application.yaml

--- src/main/java/com/tsvyk/kafkademo/controllers/KafkaController.java
+++ src/main/java/com/tsvyk/kafkademo/controllers/KafkaController.java
@@ -1,6 +1,7 @@
 package com.tsvyk.kafkademo.controllers;
 
 import com.tsvyk.kafkademo.services.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaController {
 
-    private final ProducerService producerService;
-
-    public KafkaController(ProducerService producerService) {
-        this.producerService = producerService;
-    }
+    @Autowired
+    private ProducerService producerService;
 
     @PostMapping("/publish")
     public void sendMessage(@RequestParam("message") String message) {
